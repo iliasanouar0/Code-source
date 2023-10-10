3765e0e24bdca05d58260f1a2e81db9c409c55f3
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-10 10:40:05 +0100

    added

--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ wsp.on('connection', ws => {
   ws.on('message', message => {
     console.log(`received : ${message}`);
     message
-    let response = express.response
+    let response = app.response
     let request = { body: message }
     processManager.updateProcess(request, response)
   })
