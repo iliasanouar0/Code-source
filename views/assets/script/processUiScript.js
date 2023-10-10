c48e729fb1fa36c81da9322dc0b75422dfb52652
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-10 15:42:54 +0100

    added

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -194,8 +194,14 @@ $(document).on('click', '.stop', event => {
 $(document).on('click', '.status', event => {
     const id = $(event.target)[0].attributes[2].value
     let children = $(event.target).parent().parent()[0].children
-    console.log(children);
-    console.log(id);
+    $('.count').html(children[1].innerHTML)
+    if (children[5].innerHTML == "RUNNING") {
+        $('.status').addClass("bg-success")
+        $('.status').html(children[5].innerHTML)
+    } else {
+        $('.status').addClass("bg-danger")
+        $('.status').html(children[5].innerHTML)
+    }
     $('#p_s').html(id)
     $('#modal-process-view').modal('show')
 
