fdd1142b2c59d8bc28c237afc6ff506f5b2bfd96
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-10 09:19:08 +0100

    added

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -94,7 +94,7 @@ $(document).on('click', "#p_add", () => {
 
 $(document).on('click', '.start', event => {
     const id = $(event.target)[0].attributes[2].value
-    let td = $(event.target).parent().child()
+    let td = $(event.target).parent()
     console.log(td);
     console.log(id);
     const status = "RUNNING"
