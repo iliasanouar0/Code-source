456b6cd5584d2e82abadb53bd7b253842840d1f4
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-10 11:21:26 +0100

    added

--- server/managers/processManager.js
+++ server/managers/processManager.js
@@ -45,18 +45,18 @@ const getAllData = (request, response) => {
  * @returns
  */
 const updateProcess = (data) => {
-    // let results = ""
-    // let query = "UPDATE process SET status=($1), start_in=($2) WHERE id_process=($3)"
-    // let values = [data.status, data.start_in, data.id_process]
-    // let obj = { query: query, data: values }
-    // pool.query(obj.query, obj.data, (error, result) => {
-    //     if (error) {
-    //         console.log({ name: error.name, stack: error.stack, message: error.message, error: error })
-    //         return
-    //     }
-    //     results = 'Process started successfully'
-    // })
-    return data
+    let results = ""
+    let query = "UPDATE process SET status=($1), start_in=($2) WHERE id_process=($3)"
+    let values = [data.status, data.start_in, data.id_process]
+    let obj = { query: query, data: values }
+    pool.query(obj.query, obj.data, (error, result) => {
+        if (error) {
+            console.log({ name: error.name, stack: error.stack, message: error.message, error: error })
+            return
+        }
+        results = 'Process started successfully'
+    })
+    return results
 }
 
 // const updateProcess = (request, response) => {
