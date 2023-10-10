dbd5cd303d1d3572c4f6f9deca2224ad1076367c
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-10 17:03:09 +0100

    added

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -204,7 +204,7 @@ const createRowProcessSeeds = data => {
       <td>${element.isp}</td>
       <td>idel</td>
       <td>00:00:00</td>
-      <td>
+      <td class="text-center">
       <button type="button" class="btn btn-primary details" data-id="${element.id_seeds}"><i class="far fa-eye"></i></button>
       </td></tr>`
         rows += tr
