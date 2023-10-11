d4bbd57db3e99a98a79bba0f91d7c03fc481c105
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-11 11:35:44 +0100

    adding functions

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -168,6 +168,8 @@ function socketUpdate(websocket, sendMessage, obj, pingInterval) {
             return response.json();
         }).then((data) => {
             Process_data.innerHTML = createRowProcess(data)
+        }).then(() => {
+            websocket.close()
         })
     };
     websocket.onclose = (e) => {
