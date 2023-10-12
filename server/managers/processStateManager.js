2b39343c22571ffcca6701cde8d10fc8d26c971f
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-12 15:12:22 +0100

    adding functions

--- server/managers/processStateManager.js
+++ server/managers/processStateManager.js
@@ -25,15 +25,9 @@ const getState = async (id) => {
     //     }
     //     return result.rows;
     // })
-    const results = pool.query(sql, data, (error, result) => {
-        if (error) {
-            return error;
-        }
-        return result.rows;
-    })
-    return results
-    // const list = await client.query(sql, data);
-    // return list.rows;
+    const client = await pool.connect()
+    const list = await client.query(sql, data);
+    return list.rows;
 }
 
 const updateState = (data) => {
