807fcfaa60bfb88e462018a8783becde5138ef33
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-12 13:44:18 +0100

    adding functions

--- server/index.js
+++ server/index.js
@@ -13,25 +13,25 @@ const wss = new WebSocket.Server({ port: 7073 })
 
 const wsv = new WebSocket.Server({
   port: 7074,
-  // perMessageDeflate: {
-  //   zlibDeflateOptions: {
-  //     // See zlib defaults.
-  //     chunkSize: 1024,
-  //     memLevel: 7,
-  //     level: 3
-  //   },
-  //   zlibInflateOptions: {
-  //     chunkSize: 10 * 1024
-  //   },
-  //   // Other options settable:
-  //   // clientNoContextTakeover: true, // Defaults to negotiated value.
-  //   // serverNoContextTakeover: true, // Defaults to negotiated value.
-  //   serverMaxWindowBits: 20, // Defaults to negotiated value.
-  //   // Below options specified as default values.
-  //   concurrencyLimit: 20, // Limits zlib concurrency for perf.
-  //   threshold: 1024 // Size (in bytes) below which messages
-  //   // should not be compressed if context takeover is disabled.
-  // }
+  perMessageDeflate: {
+    // zlibDeflateOptions: {
+    //   // See zlib defaults.
+    //   chunkSize: 1024,
+    //   memLevel: 7,
+    //   level: 3
+    // },
+    // zlibInflateOptions: {
+    //   chunkSize: 10 * 1024
+    // },
+    // Other options settable:
+    // clientNoContextTakeover: true, // Defaults to negotiated value.
+    // serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 20, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 20, // Limits zlib concurrency for perf.
+    // threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed if context takeover is disabled.
+  }
 });
 
 const userManager = require("./managers/userManager");
