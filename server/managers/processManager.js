2fbcf856358b1ef86d8611ccd210d036833763bb
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-13 12:43:10 +0100

    adding functions

--- server/managers/processManager.js
+++ server/managers/processManager.js
@@ -42,7 +42,7 @@ const getAllData = (request, response) => {
 
 const getAllProcessSeeds = (request, response) => {
     const id = (request.params.id)
-    let sql = "SELECT process.id_process, seeds.* FROM process JOIN seeds ON seeds.id_list=process.id_list WHERE process.id_process=$1 GROUP BY seeds.id_list,process.id_list,process.id_process,seeds.id_seeds ORDER BY CASE WHEN seeds.status = 'running' then 1 WHEN seeds.status = 'waiting' then 2  WHEN seeds.status = 'failed' then 3 WHEN seeds.status='stopped' END ASC"
+    let sql = "SELECT process.id_process, seeds.* FROM process JOIN seeds ON seeds.id_list=process.id_list WHERE process.id_process=$1 GROUP BY seeds.id_list,process.id_list,process.id_process,seeds.id_seeds ORDER BY CASE WHEN seeds.status = 'running' then 1 WHEN seeds.status = 'waiting' then 2  WHEN seeds.status = 'failed' then 3 WHEN seeds.status='stopped' END"
     pool.query(sql, [id], (error, result) => {
         if (error) {
             response.status(500).send({ name: error.name, stack: error.stack, message: error.message, error: error })
