cd82c29bda24139c8f19c428b6a8f37f30e9368c
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-13 18:50:24 +0100

    adding functions

--- server/index.js
+++ server/index.js
@@ -111,10 +111,6 @@ wss.on('connection', wss => {
         toProcess.push(seeds[i])
       }
       while (process == false) {
-        if (length == count) {
-          process = true
-          return
-        }
         console.log('in enter length : ' + length);
         console.log('in enter count : ' + count);
         for (let i = 0; i < toProcess.length; i++) {
@@ -149,9 +145,10 @@ wss.on('connection', wss => {
 
         let status = { waiting: waiting - count + 3, active: toProcess.length, finished: success, failed: failed, id_process: data.id_process }
         processStateManager.updateState(status)
+        if (length == count) {
+          break
+        }
       }
-      status = { waiting: waiting - count + 3, active: toProcess.length, finished: success, failed: failed, id_process: data.id_process }
-      processStateManager.updateState(status)
 
     } else if (request == "resume") {
       let seeds = await processManager.getAllProcessSeedsByState({ id_process: data.id_process, status: "stopped" })
