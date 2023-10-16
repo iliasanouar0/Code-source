9915d01d2a977fcfd0cb953324947580dd7b7be3
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-16 16:51:59 +0100

    adding functions

--- server/processes/gmailManagement.js
+++ server/processes/gmailManagement.js
@@ -8,7 +8,7 @@ const login = async (gmail, password) => {
     await page.goto('https://gmail.com/')
     await navigationPromise
     await page.screenshot({
-        path: '../../views/assets/images/process_result/screenshot.jpg'
+        path: './Code-source/views/assets/images/process_result/screenshot.jpg'
     });
     await page.waitForSelector('input[type="email"]')
     await page.click('input[type="email"]')
