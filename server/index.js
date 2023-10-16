72ad17521149fc723d59509826373da32718083d
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-16 17:35:56 +0100

    adding functions

--- server/index.js
+++ server/index.js
@@ -119,6 +119,8 @@ wss.on('connection', wss => {
       }
       let state = processManager.getProcessState(data.id_process)
       while (toProcess.length != 0 && state != 'STOPPED') {
+        console.log(length);
+        console.log(count);
         for (let i = 0; i < toProcess.length; i++) {
           console.log(toProcess);
           if (typeof (toProcess[i])) {
