a0d12c32692825675efc5737b73d59e2099c4803
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-16 12:59:20 +0100

    adding functions

--- server/index.js
+++ server/index.js
@@ -145,13 +145,18 @@ wss.on('connection', wss => {
             }
           }
         }
-        let w = waiting - count + 3
-        if (w < 0) {
+        if (waiting == 0) {
           let status = { waiting: 0, active: toProcess.length, finished: success, failed: failed, id_process: data.id_process }
           processStateManager.updateState(status)
         } else {
-          let status = { waiting: w, active: toProcess.length, finished: success, failed: failed, id_process: data.id_process }
-          processStateManager.updateState(status)
+          let w = waiting - count + 3
+          if (w < 0) {
+            let status = { waiting: 0, active: toProcess.length, finished: success, failed: failed, id_process: data.id_process }
+            processStateManager.updateState(status)
+          } else {
+            let status = { waiting: w, active: toProcess.length, finished: success, failed: failed, id_process: data.id_process }
+            processStateManager.updateState(status)
+          }
         }
         state = processManager.getProcessState(data.id_process)
         if (toProcess.length == 0) {
