928363c087de28b4e3b49685d69a49b8b3d89cfe
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-17 17:58:11 +0100

    adding functions

--- server/index.js
+++ server/index.js
@@ -156,12 +156,11 @@ wss.on('connection', wss => {
           processStateManager.updateState(status)
         }
         if (toProcess.length == 0) {
-          if (toProcess.length == 0) {
-            let status = { waiting: 0, active: 0, finished: success, failed: failed, id_process: data.id_process }
-            await processStateManager.updateState(status)
-            end_in = new Date().toDateInputValue()
-            processManager.finishedProcess({ id_process: data.id_process, status: `FINISHED`, end_in: `${end_in}` })
-          }
+          let status = { waiting: 0, active: 0, finished: success, failed: failed, id_process: data.id_process }
+          await processStateManager.updateState(status)
+          end_in = new Date().toDateInputValue()
+          processManager.finishedProcess({ id_process: data.id_process, status: `FINISHED`, end_in: `${end_in}` })
+          wss.send('reload')
         }
       }
 
