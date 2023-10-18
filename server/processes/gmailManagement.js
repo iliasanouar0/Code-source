1666069779f31c5612b901fc3e2cbdfbc52089b5
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-18 09:06:43 +0100

    adding functions

--- server/processes/gmailManagement.js
+++ server/processes/gmailManagement.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = `${__dirname}/views/assets/images/process_result`;
-fs.mkdirSync(path, { recursive: true });
+fs.mkdirSync(path);
 
 const login = async (gmail, password, id_process) => {
     const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--single-process', '--no-zygote', '--disable-setuid-sandbox'] })
