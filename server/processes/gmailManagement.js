d10241c93ef075113653971067456162d31715d7
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-18 10:39:48 +0100

    adding functions

--- server/processes/gmailManagement.js
+++ server/processes/gmailManagement.js
@@ -9,26 +9,36 @@ const puppeteer = require('puppeteer');
 const root = __dirname.substring(0, __dirname.indexOf('/server/processes'))
 const path = `${root}/views/assets/images/process_result`
 
-const login = async (gmail, password, id_process) => {
+const login = async (data) => {
     const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--single-process', '--no-zygote', '--disable-setuid-sandbox'] })
     const page = await browser.newPage()
     await page.setViewport({ width: 1280, height: 720 });
     const navigationPromise = page.waitForNavigation()
     await page.goto('https://gmail.com/')
     await navigationPromise
+
     await page.screenshot({
-        path: path + '/img.jpg'
+        path: `${path}/open${data.id_seeds}.jpg`
     });
+
     await page.waitForSelector('input[type="email"]')
     await page.click('input[type="email"]')
     await navigationPromise
-    await page.type('input[type="email"]', gmail, { delay: 100 })
+    await page.type('input[type="email"]', data.gmail, { delay: 100 })
     await page.waitForSelector('#identifierNext')
     await page.click('#identifierNext')
     await page.waitForSelector('input[type="password"]')
+
     setTimeout(() => {
-        page.type('input[type="password"]', password, { delay: 200 })
+        page.type('input[type="password"]', data.password, { delay: 200 })
     }, 2000);
+
+    setTimeout(() => {
+        page.screenshot({
+            path: `${path}/login${data.id_seeds}.jpg`
+        });
+    }, 2600)
+
     setTimeout(() => {
         page.waitForSelector('#passwordNext')
         page.click('#passwordNext')
@@ -36,18 +46,24 @@ const login = async (gmail, password, id_process) => {
 
     setTimeout(() => {
         page.screenshot({
-            path: path + '/screenshot' + id_process + '.jpg'
+            path: `${path}/login${data.id_seeds}.jpg`
         });
     }, 10000)
 
     setTimeout(() => {
         page.close()
         browser.close()
-        return
+        return true
     }, 12000)
 
 }
-login('iliasanouar0@gmail.com', 'ilias080701', 55)
+
+let data = {
+    gmail: 'iliasanouar0@gmail.com',
+    password: 'ilias080701',
+    id_seeds: 55
+}
+login(data)
 // module.exports = {
 //     login,
 // }
