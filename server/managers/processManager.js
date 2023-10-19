58df6fd8177fe3c7cd90894e273244efa75e9e99
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-19 17:19:53 +0100

    adding functions

--- server/managers/processManager.js
+++ server/managers/processManager.js
@@ -60,7 +60,7 @@ const getAllProcessSeedsCount = (request, response) => {
         if (error) {
             response.status(500).send({ name: error.name, stack: error.stack, message: error.message, error: error })
         }
-        response.status(200).send(4)
+        response.status(200).send(`${result.rowCount}`)
     })
 }
 
