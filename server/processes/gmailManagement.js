dd92f1bc3fea9e290bed24c2c7c8b020a8e20229
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-19 13:22:26 +0100

    adding functions

--- server/processes/gmailManagement.js
+++ server/processes/gmailManagement.js
@@ -12,7 +12,7 @@ const root = __dirname.substring(0, __dirname.indexOf('/server/processes'))
 const path = `${root}/views/assets/images/process_result`
 
 const login = async (data) => {
-    const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--single-process', '--no-zygote', '--disable-setuid-sandbox'] })
+    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--single-process', '--no-zygote', '--disable-setuid-sandbox'] })
     const page = await browser.newPage()
     await page.setViewport({ width: 1280, height: 720 });
     const navigationPromise = page.waitForNavigation()
@@ -29,17 +29,10 @@ const login = async (data) => {
     await page.click('#identifierNext')
     await navigationPromise
     await time(1000)
-    // document.querySelector("")
-    if (await page.$('[aria-invalid="true"]') != null || await page.$('#next > div > div > a') != null || await page.$('#view_container > div > div > div.pwWryf.bxPAYd > div > div.zQJV3 > div > div.qhFLie > div > div > button') != null) {
+    if (await page.$('[aria-invalid="true"]') != null || await page.$('#next > div > div > a') != null) {
         await page.screenshot({
             path: `${path}/${data.gmail.split('@')[0]}-@-invalidEmail-${data.id_process}.png`
         });
-        // if (await page.$('#next > div > div > a') != null) {
-        //     console.log('the seed is exist but 0');
-        //     await page.close()
-        //     await browser.close()
-        //     return feedBack = `${data.gmail.split('@')[0]}-@-open-${data.id_process}.png, ${data.gmail.split('@')[0]}-@-invalidEmail-${data.id_process}.png`
-        // }
         await page.close()
         await browser.close()
         return feedBack = `${data.gmail.split('@')[0]}-@-open-${data.id_process}.png, ${data.gmail.split('@')[0]}-@-invalidEmail-${data.id_process}.png`
@@ -70,6 +63,13 @@ const login = async (data) => {
     return feedBack = `${data.gmail.split('@')[0]}-@-open-${data.id_process}.png, ${data.gmail.split('@')[0]}-@-login-${data.id_process}.png`
 }
 
+// let data = {
+//     gmail: 'j965408@gmail.com',
+//     password: '11111111',
+//     id_process: 44
+// }
+
+// login(data)
 module.exports = {
     login,
 }
