ab52ce4dc5602b02fe259ccb4bc53955a7e24cd6
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-19 17:10:30 +0100

    adding functions

--- server/index.js
+++ server/index.js
@@ -293,7 +293,7 @@ app.delete("/seeds/:id", seedManager.deleteSeed);
 app.post("/process/", processManager.addProcess)
 app.get("/process/admin", processManager.getAllData)
 app.get("/process/seeds/:id", processManager.getAllProcessSeeds)
-app.get('process/page/:id', processManager.getAllProcessSeedsCount)
+app.get('/process/page/:id', processManager.getAllProcessSeedsCount)
 
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
