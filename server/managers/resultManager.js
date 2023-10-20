88c5d2cf5a9b51449ec591c501f561a1a07941b6
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-20 16:51:53 +0100

    adding functions

--- server/managers/resultManager.js
+++ server/managers/resultManager.js
@@ -15,12 +15,6 @@ const saveResult = async (data) => {
     });
     let id = generator.get();
     let values = [data.id_list, data.id_seeds, data.id_process, id, data.feedback, data.start_in, data.end_in]
-    // pool.query(sql, values, (error, result) => {
-    //     if (error) {
-    //         return { name: error.name, stack: error.stack, message: error.message }
-    //     }
-    //     return true
-    // })
     const client = await pool.connect()
     client.query(sql, values, (err) => {
         if (err) {
