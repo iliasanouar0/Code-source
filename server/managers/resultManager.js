1e651643ca2c986373429dbbf5bcfd120d959fc7
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-20 15:25:48 +0100

    adding functions

--- server/managers/resultManager.js
+++ server/managers/resultManager.js
@@ -7,7 +7,7 @@ let config = data.data
 const pool = new pg.Pool(config);
 
 
-const saveResult = (data) => {
+const saveResult = async (data) => {
     let sql = `INSERT INTO results (id_list, id_seeds, id_process, id_result, feedback, start_in, end_in) VALUES ($1, $2, $3, $4, $5, $6, $7)`
     var generator = new IdGenerator({
         len: 4,
@@ -15,10 +15,18 @@ const saveResult = (data) => {
     });
     let id = generator.get();
     let values = [data.id_list, data.id_seeds, data.id_process, id, data.feedback, data.start_in, data.end_in]
-    pool.query(sql, values, (error, result) => {
-        if (error) {
-            return { name: error.name, stack: error.stack, message: error.message }
+    // pool.query(sql, values, (error, result) => {
+    //     if (error) {
+    //         return { name: error.name, stack: error.stack, message: error.message }
+    //     }
+    //     return true
+    // })
+    const client = await pool.connect()
+    client.query(sql, values, (err) => {
+        if (err) {
+            return err;
         }
+        client.release()
         return true
     })
 }
