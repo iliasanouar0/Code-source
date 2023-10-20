9b0b470abfabb7af19ef1ad85cbe34f3ad0bc01a
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-20 18:26:26 +0100

    adding functions

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -421,8 +421,7 @@ $(document).on('click', '.details', event => {
         console.log(data);
         console.log(data.length);
         console.log(data[0]);
-        console.log(data[0].feedback);
-        if (data.length === 0 || data[0].feedBack === 0) {
+        if (data.length == 0 || data[0].feedBack == 0) {
             console.log('no');
             Swal.fire({
                 title: 'NO feedback yet !!',
