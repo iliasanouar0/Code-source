d3c4cbf673dc8e203f42fdf63a196422e4704701
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-23 18:31:52 +0100

    adding functions

--- server/managers/processManager.js
+++ server/managers/processManager.js
@@ -117,6 +117,18 @@ const finishedProcess = (data) => {
     return true
 }
 
+const restedProcess = (data) => {
+    let query = "UPDATE process SET status=($1),start_in=($2),end_in=($3) WHERE id_process=($4)"
+    let values = [data.status, data.start_in, data.end_in, data.id_process]
+    let obj = { query: query, data: values }
+    pool.query(obj.query, obj.data, (error, result) => {
+        if (error) {
+            return `${error.name, error.stack, error.message, error}`
+        }
+    })
+    return true
+}
+
 const stoppedProcess = (data) => {
     let query = "UPDATE process SET status=($1) WHERE id_process=($2)"
     let values = [data.status, data.id_process]
