4b94cee5a9435fbaa61d9817f08b6455d8d298f7
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-24 15:37:08 +0100

    adding functions

--- server/managers/processManager.js
+++ server/managers/processManager.js
@@ -74,7 +74,7 @@ const getAllProcessSeedsServer = async (id) => {
 const getAllProcessSeedsByState = async (data) => {
     let values = [data.id_process, data.status]
     // let sql = "SELECT process.id_process,process.action, results.status as rs,seeds.* FROM process JOIN results ON results.id_process=process.id_process JOIN seeds ON seeds.id_list=process.id_list WHERE process.id_process=($1) AND results.status=($2) GROUP BY seeds.id_list,process.id_list,process.id_process,seeds.id_seeds,results.status"
-    let sql = "SELECT process.id_process,process.action, results.status as rs FROM process JOIN results ON results.id_process=process.id_process WHERE process.id_process=($1) AND results.status=($2) GROUP BY seeds.id_list,process.id_list,process.id_process,seeds.id_seeds,results.status"
+    let sql = "SELECT process.id_process,process.action, results.status as rs FROM process JOIN results ON results.id_process=process.id_process WHERE process.id_process=($1) AND results.status=($2) GROUP BY process.id_process,results.status"
     const client = await pool.connect()
     const list = await client.query(sql, values);
     client.release()
