da5b0cc2531a35aa4694959974706e0e60ae493c
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-24 09:21:06 +0100

    adding functions

--- views/assets/script/render_admin.js
+++ views/assets/script/render_admin.js
@@ -230,7 +230,7 @@ function getData() {
           } else if (row.status == 'STOPPED') {
             return `<button type="button" class="btn btn-primary status" data-id="${row.id_process}"><i class="far fa-eye"></i></button>
 <button type="button" class="btn btn-success start"  data-id="${row.id_process}"><i class="fas fa-redo"></i></button>
-<button type="button" class="btn btn-danger stop"  data-id="${row.id_process}"><i class="fas fa-power-off"></i></button>
+<button type="button" class="btn btn-danger stop"  data-id="${row.id_process}"><i class="fas fa-power-off reset"></i></button>
 <button type="button" class="btn btn-info edit"  data-id="${row.id_process}"><i class="fas fa-edit"></i></button>`
           } else {
             return `<button type="button" class="btn btn-primary status" data-id="${row.id_process}"><i class="far fa-eye"></i></button>
