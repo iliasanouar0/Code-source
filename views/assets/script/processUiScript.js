bba251631599567a25cd351ead0f2cb984cc811d
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-25 16:57:06 +0100

    adding functions

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -187,19 +187,7 @@ $(document).on('click', '.status', event => {
     let id = $(event.target)[0].attributes[2].value
     let children = $(event.target).parent().parent()[0].children
     $('.count').html(children[2].innerHTML)
-    if (children[6].innerHTML == "RUNNING") {
-        $('.status_bg').removeClass("bg-success bg-danger bg-info")
-        $('.status_bg').addClass("badge bg-success")
-        $('.status_bg').html(children[6].innerHTML)
-    } else if (children[6].innerHTML == "STOPPED") {
-        $('.status_bg').removeClass("bg-success bg-danger bg-info")
-        $('.status_bg').addClass("badge bg-danger")
-        $('.status_bg').html(children[6].innerHTML)
-    } else {
-        $('.status_bg').removeClass("bg-success bg-danger bg-info")
-        $('.status_bg').addClass("badge bg-info")
-        $('.status_bg').html(children[6].innerHTML)
-    }
+    $('.status_bg').html(children[6].innerHTML)
     $('#p_s').html(id)
     fetch(`http://${ip}:3000/process/seeds/${id}?offset=0`, { method: "GET" }).then(response => {
         return response.json()
