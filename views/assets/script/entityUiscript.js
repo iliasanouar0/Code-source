8850ea7f472a3411ffb1c8d58560a06990e2943a
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-25 12:57:52 +0100

    adding functions

--- views/assets/script/entityUiscript.js
+++ views/assets/script/entityUiscript.js
@@ -24,7 +24,7 @@ const addEntity = (data) => {
             icon: "success",
             confirmButtonText: "ok",
         }).then(() => {
-            location.reload();
+            getDataEntity()
         });
     });
 };
