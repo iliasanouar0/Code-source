af681fa05a0235674ac45ab895ef9306d0d7159b
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-25 13:54:48 +0100

    adding functions

--- views/assets/script/render_admin.js
+++ views/assets/script/render_admin.js
@@ -629,7 +629,7 @@ function getDataUser() {
       {
         data: null,
         render: function (data, type, row) {
-          return `<div class="bg-lightblue card m-0">
+          return `<div class="bg-teal card m-0">
           <div class="card-body p-0 text-center text-light">
           ${row.isp}
           </div>
