00529b04cde31df87491ef93f64fa14cfe193152
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-25 11:44:59 +0100

    adding functions

--- views/assets/script/listUiscript.js
+++ views/assets/script/listUiscript.js
@@ -11,22 +11,25 @@ $(document).ready(function () {
     $(".update").val(new Date().toDateInputValue());
 });
 
-// let addBtn = document.querySelectorAll(".add_seeds");
-// for (let i = 0; i < addBtn.length; i++) {
-//     addBtn[i].addEventListener("click", () => {
-//         let id = addBtn[i].dataset.id;
-//         document.querySelector("#l_seeds_add").dataset.id = id;
-//         document.querySelector(".bulk").dataset.id = id;
-//         const myModal = new bootstrap.Modal(
-//             document.querySelector(".add_seeds_md")
-//         );
-//         myModal.show();
-//     });
-// }
+let addBtn = document.querySelectorAll(".add_seeds");
+for (let i = 0; i < addBtn.length; i++) {
+    addBtn[i].addEventListener("click", () => {
+        let id = addBtn[i].dataset.id;
+        document.querySelector("#l_seeds_add").dataset.id = id;
+        document.querySelector(".bulk").dataset.id = id;
+        const myModal = new bootstrap.Modal(
+            document.querySelector(".add_seeds_md")
+        );
+        myModal.show();
+    });
+}
 
 $(document).on('click', '.add_seeds', event => {
     id = $(event.target).data('id')
+    $('#l_seeds_add').data('id', id)
+    $('.bulk').data('id', id)
     console.log(id);
+    $(".add_seeds_md").modal('show')
 })
 
 $(document).on('click', '.bulk', event => {
