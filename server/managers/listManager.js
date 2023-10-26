848389288d309264bdf17a33dde657939b750de4
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-26 11:58:18 +0100

    adding functions

--- server/managers/listManager.js
+++ server/managers/listManager.js
@@ -38,7 +38,7 @@ const createList = (request, response) => {
 
 const getLists = (request, response) => {
   pool.query(
-    "SELECT list.*, COUNT(id_seeds) AS seeds_count, users.login, entity.nom FROM list LEFT JOIN seeds ON seeds.id_list = list.id_list JOIN users ON list.id_user=users.id_user JOIN entity ON users.id_entity=entity.id_entity GROUP BY 1,users.id_user,users.id_entity,entity.id_entity ORDER BY list.add_date DESC",
+    "SELECT list.*, COUNT(id_seeds) AS seeds_count, users.login, entity.nom FROM list LEFT JOIN seeds ON seeds.id_list = list.id_list JOIN users ON list.id_user=users.id_user JOIN entity ON users.id_entity=entity.id_entity GROUP BY 1,users.id_user,users.id_entity,entity.id_entity ORDER BY list.date_add DESC",
     (error, results) => {
       if (error) {
         response.status(500).json(error.message);
