f26d54009cebb65d0d7683496b28ee78a5e15f0b
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-27 12:28:10 +0100

    adding functions

--- views/assets/script/ifram.js
+++ views/assets/script/ifram.js
@@ -1,5 +1,5 @@
 $(document).on('click', '#add_frame', event => {
     console.log('working on it');
-    $('<iframe src="http://209.170.73.224/NewReportingApp/views/mailer/process/" frameborder="0" id="myFrame"></iframe>').appendTo('.newFrame');
+    $('<iframe src="http://209.170.73.224/NewReportingApp/views/mailer/process/" frameborder="0" id="myFrame" width="1400px"></iframe>').appendTo('.newFrame');
     $(event.target).remove()
 })
