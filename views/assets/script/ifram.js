c00aca79bb3c00e79094e1f1d38477b1117b104b
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-27 12:18:31 +0100

    adding functions

--- views/assets/script/ifram.js
+++ views/assets/script/ifram.js
@@ -1,4 +1,4 @@
 $(document).on('click', '#add_frame', () => {
     console.log('working on it');
-    $('<iframe src="http://google.com" frameborder="0" scrolling="no" id="myFrame"></iframe>').appendTo('.accordion');
+    $('<iframe src="http://google.com" frameborder="0" scrolling="no" id="myFrame"></iframe>').appendTo('.newFrame');
 })
