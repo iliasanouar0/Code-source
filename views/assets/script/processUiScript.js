e4622e6d25edc953481512c86ba01bce1bcb7dbf
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-30 18:39:37 +0000

    adding functions

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -532,4 +532,5 @@ $(document).on('click', '.delete-all-this', () => {
 })
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+console.log(tooltipTriggerList);
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
