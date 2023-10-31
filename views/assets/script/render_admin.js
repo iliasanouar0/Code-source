59b1bd94c28bd3041451eecfa827e87c7d5d453f
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 09:34:38 +0000

    adding functions

--- views/assets/script/render_admin.js
+++ views/assets/script/render_admin.js
@@ -611,15 +611,16 @@ if (path.includes("/admin/users/")) {
         });
       });
   });
-  getData
-  $('[data-bs-toggle="tooltip"]').tooltip();
-  console.log($('[data-bs-toggle="tooltip"]').tooltip());
-  console.log($('[data-bs-toggle="tooltip"]'))
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  console.log(tooltipTriggerList);
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  });
+  await Promise.all([getData]).then(() => {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+    console.log($('[data-bs-toggle="tooltip"]').tooltip());
+    console.log($('[data-bs-toggle="tooltip"]'))
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    console.log(tooltipTriggerList);
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+  })
 } else if (path.includes("/admin/lists/")) {
   getDatalist
 }
