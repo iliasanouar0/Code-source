a3f55181307cd917b49b486347cf02db3f5d6833
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 17:39:01 +0000

    adding functions

--- views/assets/script/render_admin.js
+++ views/assets/script/render_admin.js
@@ -618,12 +618,7 @@ const getDataSettings = () => {
             tables.push({ table_name: elm.tablename, data: data })
           })
       })
-      // console.log(tables);
-      // tables.forEach(table => {
-      //   console.log(table);
-      //   let table_name = table.table_name
-      //   console.log(table_name);
-      // })
+    }).then(() => {
       for (let i = 0; i < tables.length; i++) {
         console.log(tables[i]);
       }
