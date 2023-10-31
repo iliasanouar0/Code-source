2202339eadec53508ca9a4a7a635e16b31f5e6e3
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 16:59:01 +0000

    adding functions

--- server/managers/settingsManager.js
+++ server/managers/settingsManager.js
@@ -9,7 +9,7 @@ const getTables = (request, response) => {
     let result = []
     let sql1 = `SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'`
     // let sql = `SELECT tablename,information_schema.columns.table_name,information_schema.columns.column_name,information_schema.columns.data_type FROM pg_catalog.pg_tables JOIN information_schema.columns ON information_schema.columns.table_name=pg_catalog.pg_tables.tablename WHERE schemaname != 'pg_catalog' AND information_schema.columns.table_schema='public'`
-    let sql2 = `SELECT  table_name,  column_name,  data_type  FROM  information_schema.columns WHERE table_name='users' GROUP BY table_name`
+    let sql2 = `SELECT  table_name,  column_name,  data_type  FROM  information_schema.columns WHERE table_name='users' GROUP BY table_name,columns.column_name`
 
 
     let sql = `SELECT * FROM (SELECT tablename,tableowner,schemaname FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema' GROUP BY pg_tables.tablename,pg_tables.schemaname,pg_tables.tableowner UNION SELECT  table_name,  column_name,  data_type  FROM  information_schema.columns GROUP BY columns.column_name,columns.table_name,columns.data_type) AS U`
