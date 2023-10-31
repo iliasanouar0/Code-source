0415ad41592e8d86d66bb32dc2be1dbe6fbfff6e
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 15:49:53 +0000

    adding functions

--- server/managers/settingsManager.js
+++ server/managers/settingsManager.js
@@ -7,7 +7,8 @@ const pool = new pg.Pool(config);
 
 const getTables = (request, response) => {
     let result = []
-    let sql = `SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'`
+    let sql = `SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog'`
+    // let sql = `SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'`
     let data = `SELECT  table_name,  column_name,  data_type  FROM  information_schema.columns WHERE  table_name = $1`
     pool.query(sql, (err, res) => {
         if (err) {
