07f7cf6c3819ce2d31bcc398fe48ed153f24eca5
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 15:45:03 +0000

    adding functions

--- server/managers/settingsManager.js
+++ server/managers/settingsManager.js
@@ -6,16 +6,22 @@ let config = data.data
 const pool = new pg.Pool(config);
 
 const getTables = (request, response) => {
-    let names = []
+    let result = []
     sql = `SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'`
+    let data = `SELECT  table_name,  column_name,  data_type  FROM  information_schema.columns WHERE  table_name = $1`
     pool.query(sql, (err, res) => {
         if (err) {
             response.status(500).send(err.details)
         }
         res.rows.forEach(elm => {
-            names.push(elm.tablename)
+            pool.query(data, [elm.tablename], (err, res) => {
+                if (err) {
+                    response.status(500).send(err.details)
+                }
+                result.push(res.rows[0])
+            })
         })
-        response.send(names)
+        response.send(result)
     })
 }
 
