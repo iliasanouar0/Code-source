b1a604617daa0989ffec38ce0431ac9aa56e6b68
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 16:52:11 +0000

    adding functions

--- server/managers/settingsManager.js
+++ server/managers/settingsManager.js
@@ -10,7 +10,7 @@ const getTables = (request, response) => {
     let sql1 = `SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'`
     // let sql = `SELECT tablename,information_schema.columns.table_name,information_schema.columns.column_name,information_schema.columns.data_type FROM pg_catalog.pg_tables JOIN information_schema.columns ON information_schema.columns.table_name=pg_catalog.pg_tables.tablename WHERE schemaname != 'pg_catalog' AND information_schema.columns.table_schema='public'`
     let sql2 = `SELECT  table_name,  column_name,  data_type  FROM  information_schema.columns `
-    let sql = `SELECT * FROM (SELECT tablename,tableowner,schemaname FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema' GROUP BY pg_tables.tablename,pg_tables.schemaname,pg_tables.tableowner UNION ALL SELECT  table_name,  column_name,  data_type  FROM  information_schema.columns GROUP BY columns.column_name,columns.table_name,columns.data_type) AS U  `
+    let sql = `SELECT * FROM (SELECT tablename,tableowner,schemaname FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema' GROUP BY pg_tables.tablename,pg_tables.schemaname,pg_tables.tableowner UNION SELECT  table_name,  column_name,  data_type  FROM  information_schema.columns GROUP BY columns.column_name,columns.table_name,columns.data_type) AS U  `
     pool.query(sql, (err, res) => {
         if (err) {
             response.status(500).send({ name: err.name, message: err.message, stack: err.stack })
