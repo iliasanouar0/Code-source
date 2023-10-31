ada8247d6e16dd6562d7fc9236709b32f9d72ab3
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 16:30:53 +0000

    adding functions

--- server/managers/settingsManager.js
+++ server/managers/settingsManager.js
@@ -7,10 +7,9 @@ const pool = new pg.Pool(config);
 
 const getTables = (request, response) => {
     let result = []
-    // let sql = `SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog'`
-    let sql = `SELECT tablename,information_schema.columns.table_name,information_schema.columns.column_name,information_schema.columns.data_type FROM pg_catalog.pg_tables JOIN information_schema.columns ON information_schema.columns.table_name=pg_catalog.pg_tables.tablename WHERE schemaname != 'pg_catalog' AND information_schema.columns.table_schema='public'`
-    let data = `SELECT  *  FROM  information_schema.columns`
-    // let data = `SELECT  table_name,  column_name,  data_type  FROM  information_schema.columns `
+    let sql = `SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';`
+    // let sql = `SELECT tablename,information_schema.columns.table_name,information_schema.columns.column_name,information_schema.columns.data_type FROM pg_catalog.pg_tables JOIN information_schema.columns ON information_schema.columns.table_name=pg_catalog.pg_tables.tablename WHERE schemaname != 'pg_catalog' AND information_schema.columns.table_schema='public'`
+    let data = `SELECT  table_name,  column_name,  data_type  FROM  information_schema.columns `
     pool.query(sql, (err, res) => {
         if (err) {
             response.status(500).send({ name: err.name, message: err.message, stack: err.stack })
