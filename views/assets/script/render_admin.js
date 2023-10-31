25312dbfbba2140d800657dadd430f09c6bc0445
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 17:30:07 +0000

    adding functions

--- views/assets/script/render_admin.js
+++ views/assets/script/render_admin.js
@@ -565,20 +565,63 @@ const getDataUser = $("#userTable").DataTable({
   ],
 })
 
+let lay = `
+<div class="card card-primary card-outline">
+<a class="d-block w-100 collapsed" data-toggle="collapse" href="#${table_name}"
+    aria-expanded="false">
+    <div class="card-header">
+        <h4 class="card-title w-100">
+            ${table_name}
+        </h4 >
+    </div >
+</a >
+  <div id="${table_name}" class="collapse" data-parent="#accordion">
+    <div class="card-body">
+      <div class="row text-right mb-3">
+        <div class="col">
+          <button class="btn btn-primary">action-1</button>
+          <button class="btn btn-primary">action-2</button>
+          <button class="btn btn-primary">action-3</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Column name</th>
+                <th scope="col">Data type</th>
+              </tr>
+            </thead>
+            <tbody class="columns">
+            ${rows}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div >
+
+
+  `
+
+
 const getDataSettings = () => {
+  let tables = []
   fetch(`http://${ip}:3000/settings/tables/`, { method: "GET" })
     .then(response => {
       return response.json()
     }).then(data => {
       data.forEach(elm => {
         let table_name = elm.tablename
-        console.log(table_name);
         fetch(`http://${ip}:3000/settings/columns/${table_name}`, { method: "GET" })
           .then(response => {
             return response.json()
           }).then(data => {
-            console.log(data);
+            tables.push({ table_name: table_name, data: data })
           })
+        console.log(tables);
       })
     })
 }
