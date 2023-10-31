32c60873a30f42b65bd77570516dbfc1b8a247de
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 18:42:32 +0000

    adding functions

--- views/assets/script/settingUiscript.js
+++ views/assets/script/settingUiscript.js
@@ -9,3 +9,33 @@ $(document).on('click', '.switch_inp', () => {
     $('.input_way, .text_way').toggle(200)
     $('.switch_inp i').toggleClass(`fa-keyboard fa-list`)
 })
+
+$(function () {
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $("#sqlCreateTable").autocomplete({
+        source: availableTags
+    });
+});
