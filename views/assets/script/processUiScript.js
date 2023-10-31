8eaea8577e5a9e07df284d25024a7f39a9210bde
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 09:28:30 +0000

    adding functions

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -23,6 +23,7 @@ $(document).ready(() => {
 })
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+console.log(tooltipTriggerList);
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 });
