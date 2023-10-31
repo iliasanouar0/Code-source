4ab6274b79c7e985e26c20d2ca8b15d044f15131
iliasanouar0 <iliasanouar0@gmail.com>
2023-10-31 18:10:41 +0000

    adding functions

--- views/assets/script/settingUiscript.js
+++ views/assets/script/settingUiscript.js
@@ -4,3 +4,8 @@ console.log('welcome to settings');
 $(document).on('click', '#add_table', () => {
     $('.add_table').modal('show')
 })
+
+$(document).on('click', '.switch_inp', () => {
+    $('.input_way, .text_way').toggle(200)
+    $('.switch_inp i').toggleClass(`fa-keyboard fa-list`)
+})
