dd6871fab66cf2952b8c065b83527055f15555b3
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-02 09:40:53 +0000

    adding functions

--- views/assets/script/settingUiscript.js
+++ views/assets/script/settingUiscript.js
@@ -414,9 +414,6 @@ $(document).on('click', '.delete', event => {
                 body: { sql: `${sql}` },
                 headers: {
                     'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
-                    'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS,PATCH'
                 }
             }).then(response => {
                 return response.text()
