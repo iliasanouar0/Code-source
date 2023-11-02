c09d326fb29e6bf1cf9b397e5f1995e49097a36e
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-02 18:57:43 +0000

    adding functions

--- server/test.js
+++ server/test.js
@@ -1,8 +1,17 @@
+const fs = require('fs')
 let dotenv = require('dotenv')
+
 const result = dotenv.config()
-process.env.NODE_ENV = "production"
+let mode = process.env.NODE_ENV
+
+let test = mode == "development" ? "production" : "development"
+
+console.log(mode);
 if (result.error) {
   throw result.error
 }
+
+fs.writeFile('./.env', '', function () { console.log('done') })
+fs.writeFile('./.env', `NODE_ENV=${test}`, function () { console.log('done') })
 
 console.log(result.parsed)
