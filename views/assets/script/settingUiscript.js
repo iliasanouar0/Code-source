b2079d466722953ea0b3d2b5efd37a9f4e5c367d
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-02 11:21:33 +0000

    adding functions

--- views/assets/script/settingUiscript.js
+++ views/assets/script/settingUiscript.js
@@ -1,6 +1,10 @@
 let user = JSON.parse(sessionStorage.user);
 console.log('welcome to settings');
 
+$(document).on('click', '#refresh', () => {
+    getDataSettings()
+})
+
 $(document).on('click', '#add_table', () => {
     $('.add_table').modal('show')
 })
