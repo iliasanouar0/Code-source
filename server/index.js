7b4e9a31096266aa765db010b1a595f60b3165d2
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-03 16:31:57 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -46,11 +46,11 @@ app.options("*", cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 app.get('/ap/ip/', (req, res) => {
   res.status(200).send(req.ip)
 })
