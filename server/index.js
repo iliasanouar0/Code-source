07395e95e14cf2969bc28fd975ba632d812faac2
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-03 16:44:56 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
 // app.use(ipfilter(ips, { mode: 'allow' }))
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
   next();
 });
 app.get('/ap/ip/', (req, res) => {
