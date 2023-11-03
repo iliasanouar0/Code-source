c807610e42ff11ba0c196031fa301ce3b107a78b
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-03 09:48:24 +0000

    adding functions

--- views/assets/script/authorizationUiscript.js
+++ views/assets/script/authorizationUiscript.js
@@ -1,1 +1,19 @@
 console.log('welcome to IP authorization');
+
+$(document).on('click', '#add_ip', () => {
+    $('#entity_add').html('')
+    fetch(`http://${ip}:3000/entity`, {
+        method: "GET",
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        data.forEach((elm) => {
+            let option = document.createElement("option");
+            option.innerHTML = elm["nom"];
+            option.setAttribute("value", elm["id_entity"]);
+            $('#entity_add').append(option);
+        });
+    }).then(() => {
+        $(".add_ip").modal("show");
+    })
+})
