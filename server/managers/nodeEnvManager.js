2a48482a007848258d942e5180e973e3fe7ee344
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-06 10:25:51 +0000

    adding functions

--- server/managers/nodeEnvManager.js
+++ server/managers/nodeEnvManager.js
@@ -10,16 +10,20 @@ const getMode = (req, res) => {
 }
 
 const setMode = (req, res) => {
-    const result = dotenv.config()
-    if (result.error) {
-        throw result.error
-    }
-    let mode = result.parsed.NODE_ENV
-    let test = mode == "development" ? "production" : "development"
-    fs.writeFile('./.env', `NODE_ENV=${test}`, function () { console.log('done') })
-    res.status(200).send(test).then(() => {
+    try {
+        const result = dotenv.config()
+        if (result.error) {
+            throw result.error
+        }
+        let mode = result.parsed.NODE_ENV
+        let test = mode == "development" ? "production" : "development"
+        fs.writeFile('./.env', `NODE_ENV=${test}`, function () { console.log('done') })
+        res.status(200).send(test)
+    } catch (error) {
+        throw error
+    } finally {
         process.exit(0)
-    })
+    }
 }
 
 module.exports = {
