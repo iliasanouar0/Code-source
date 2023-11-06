2b0355063a0ac153644abcccef3e9bf10d847666
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-06 11:28:28 +0000

    adding functions

--- server/managers/nodeEnvManager.js
+++ server/managers/nodeEnvManager.js
@@ -16,7 +16,7 @@ const setMode = (req, res) => {
     }
     let mode = result.parsed.NODE_ENV
     let test = mode == "development" ? "production" : "development"
-    fs.writeFile('./.env', `NODE_ENV=${test}`, process.exit(1))
+    fs.writeFile('./.env', `NODE_ENV=${test}`)
 }
 
 module.exports = {
