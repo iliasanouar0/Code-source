5e5a01e33579f94c2e00f93a8bf5ebb6b2aad09e
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-07 10:45:47 +0000

    adding functions

--- server/managers/nodeEnvManager.js
+++ server/managers/nodeEnvManager.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 let dotenv = require('dotenv')
+const replace = require('replace-in-file');
+
 
 const getMode = (req, res) => {
-    // let ip = req.socket.remoteAddress
-    // res.status(200).send(ip)
     const result = dotenv.config()
     if (result.error) {
         throw result.error
@@ -18,7 +18,22 @@ const setMode = (req, res) => {
     }
     let mode = result.parsed.NODE_ENV
     let test = mode == "development" ? "production" : "development"
-    fs.writeFile('./.env', `NODE_ENV=${test}`, () => { console.log('done'); res.status(200).send(test); process.exit(1) })
+    // fs.writeFile('./.env', `NODE_ENV=${test}`, () => { console.log('done'); res.status(200).send(test); process.exit(1) })
+    const options = {
+        files: './.env',
+        from: `/NODE_ENV=${mode}/g`,
+        to: `NODE_ENV=${test}`,
+    };
+    try {
+        const results = replace.sync(options);
+        console.log('Replacement results:', results);
+        res.status(200).send(test)
+    }
+    catch (error) {
+        console.error('Error occurred:', error);
+    } finally {
+        process.exit(1)
+    }
 }
 
 module.exports = {
