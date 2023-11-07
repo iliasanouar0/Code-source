6ca142bfe289c0f0b44aef45a2d66246e1604c56
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-07 16:55:37 +0000

    adding functions

--- views/assets/script/userUiscript.js
+++ views/assets/script/userUiscript.js
@@ -209,13 +209,14 @@ $(document).on('click', '.edit', event => {
                 optionsEntity.item(i).setAttribute("selected", "true");
             }
         }
-        for (let i = 0; i < optionsIsp.length; i++) {
-            for (let j = 0; j < isp.length; j++) {
-                if (optionsEntity.item(i).value == isp[j]) {
-                    optionsEntity.item(i).setAttribute("selected", "true");
-                }
-            }
-        }
+        console.log(optionsEntity.item(1));
+        // for (let i = 0; i < optionsIsp.length; i++) {
+        //     for (let j = 0; j < isp.length; j++) {
+        //         if (optionsEntity.item(i).value == isp[j]) {
+        //             optionsEntity.item(i).setAttribute("selected", "true");
+        //         }
+        //     }
+        // }
         $('#edit').data('id', id)
     }).then(() => {
         $(".edit_user").modal("show");
