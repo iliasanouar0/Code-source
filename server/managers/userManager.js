60efe6019dd4ad1c26216974783c8cb640864b81
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-08 16:14:21 +0000

    adding functions

--- server/managers/userManager.js
+++ server/managers/userManager.js
@@ -54,6 +54,18 @@ const createUser = (request, response) => {
   let update = new Date()
   let hash = passwordHash.generate(obj.password, { algorithm: 'md5' })
   fs.appendFileSync('../../.password', `\n${obj.login},${obj.password}\n`)
+  fs.writeFile('../../.password', newFile, "utf8", function (err) {
+    if (err) return console.log(err)
+    fs.readFile('../../.password', (e, d) => {
+      if (e) throw e
+      let text = d.toString().split(/\r?\n/).filter(line => line.trim() !== "").join("\n");
+      console.log(text);
+      fs.writeFile('../../.password', text, "utf8", function (err) {
+        if (err) return console.log(err)
+        console.log("true")
+      })
+    })
+  })
   pool.query(
     "INSERT INTO users (f_name, l_name, login, type, password, status, date_add, date_update, id_entity, isp) VALUES ($1, $2, $3,$4,$5,$6,$7,$8, $9, $10) RETURNING id_user",
     [
@@ -129,7 +141,7 @@ const deleteUser = (request, response) => {
         if (err) return console.log(err)
         fs.readFile('../../.password', (e, d) => {
           if (e) throw e
-          let text = d.toString().split(/\r?\n/).filter(line => line.trim() !== "").join("\r\n");
+          let text = d.toString().split(/\r?\n/).filter(line => line.trim() !== "").join("\n");
           console.log(text);
           fs.writeFile('../../.password', text, "utf8", function (err) {
             if (err) return console.log(err)
