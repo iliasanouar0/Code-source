dd194f5253f9c4592272c4c604cbe81737196b51
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-09 15:35:16 +0000

    adding functions

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -373,6 +373,34 @@ $(document).on('click', '.stop', event => {
     })
 })
 
+var h1 = document.getElementsByTagName('h1')[0];
+var sec = 0;
+var min = 0;
+var hrs = 0;
+var t;
+
+function tick() {
+    sec++;
+    if (sec >= 60) {
+        sec = 0;
+        min++;
+        if (min >= 60) {
+            min = 0;
+            hrs++;
+        }
+    }
+}
+function add() {
+    tick();
+    h1.textContent = (hrs > 9 ? hrs : "0" + hrs)
+        + ":" + (min > 9 ? min : "0" + min)
+        + ":" + (sec > 9 ? sec : "0" + sec);
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+
 $(document).on('click', '#restart_s', () => {
     Swal.fire({
         title: 'Are you sure?',
@@ -386,6 +414,7 @@ $(document).on('click', '#restart_s', () => {
         if (result.isConfirmed) {
             websocket_s.send(JSON.stringify({ request: "restart" }))
             $('#restart').modal('show')
+            timer()
         } else if (result.isDismissed) {
             Swal.fire({
                 position: 'top-end',
