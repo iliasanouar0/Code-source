30b4e2976c77134c1866839ab522bdc7547793bc
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-09 16:28:07 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -273,6 +273,7 @@ wss.on('connection', (wss, req) => {
           let status = { waiting: 0, active: 0, finished: success, failed: failed, id_process: data.id_process }
           await processStateManager.updateState(status)
           processManager.finishedProcess({ id_process: data.id_process, status: `FINISHED` })
+          console.log(`process with id : ${data.id_process} Finished`);
           sendToAll(clients, 'reload')
         }
       }
