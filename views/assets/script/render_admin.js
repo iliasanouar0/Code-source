5f0a42b696a625b8a0c39992cb68abe22e6e4ef1
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-09 17:42:12 +0000

    adding functions

--- views/assets/script/render_admin.js
+++ views/assets/script/render_admin.js
@@ -746,6 +746,9 @@ const getMode = () => {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
       "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS",
+    },
+    error: (err) => {
+      location.href = '/views/access.html'
     }
   };
   $.ajax(settings).done(function (responseText) {
