d376fb3f65059a49e815f81cddde14df3214bc90
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-09 13:27:57 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -438,43 +438,45 @@ wss.on('connection', (wss, req) => {
       let ip_process = await processManager.getAllProcessByState({ status: "RUNNING" })
       if (ip_process.length != 0) {
         let stand = 0
-        for (let i = 0; i <= stand; i++) {
-          await time(5000)
-          let data = {
-            id_process: `${ip_process[i].id_process}`,
-            status: `PAUSED`,
-          }
-          processManager.stoppedProcess(data)
-          let seeds = await processManager.getAllProcessSeedsByState({ id_process: data.id_process, status: "waiting" })
-          let seedsRunning = await processManager.getAllProcessSeedsByState({ id_process: data.id_process, status: "running" })
-          let statechangeSeeds = []
-          let statechangeSeedsRunning = []
-          for (let i = 0; i < seeds.length; i++) {
-            statechangeSeeds.push({ id_seeds: seeds[i].id_seeds, id_process: data.id_process })
-          }
-          for (let i = 0; i < seedsRunning.length; i++) {
-            statechangeSeedsRunning.push({ id_seeds: seedsRunning[i].id_seeds, id_process: data.id_process })
-          }
-          await resultManager.updateState(statechangeSeeds, "paused")
-          await resultManager.updateState(statechangeSeedsRunning, "paused")
-          let state = await processStateManager.getState(data.id_process)
-          console.log(state);
-          let success = state[0].finished
-          let failed = state[0].failed
-          let status = { waiting: 0, active: 0, finished: success, failed: failed, id_process: data.id_process }
-          console.log(status);
-          await time(2000)
-          await processStateManager.updateState(status)
-          if (seeds.length == 0) {
-            processManager.processing({ action: 'kill', isp: seedsRunning[0].isp, id_process: data.id_process })
-          } else {
-            processManager.processing({ action: 'kill', isp: seeds[0].isp, id_process: data.id_process })
+        while (stand = 0 && ip_process != 0) {
+          for (let i = 0; i <= stand; i++) {
+            await time(5000)
+            let data = {
+              id_process: `${ip_process[i].id_process}`,
+              status: `PAUSED`,
+            }
+            processManager.stoppedProcess(data)
+            let seeds = await processManager.getAllProcessSeedsByState({ id_process: data.id_process, status: "waiting" })
+            let seedsRunning = await processManager.getAllProcessSeedsByState({ id_process: data.id_process, status: "running" })
+            let statechangeSeeds = []
+            let statechangeSeedsRunning = []
+            for (let i = 0; i < seeds.length; i++) {
+              statechangeSeeds.push({ id_seeds: seeds[i].id_seeds, id_process: data.id_process })
+            }
+            for (let i = 0; i < seedsRunning.length; i++) {
+              statechangeSeedsRunning.push({ id_seeds: seedsRunning[i].id_seeds, id_process: data.id_process })
+            }
+            await resultManager.updateState(statechangeSeeds, "paused")
+            await resultManager.updateState(statechangeSeedsRunning, "paused")
+            let state = await processStateManager.getState(data.id_process)
+            console.log(state);
+            let success = state[0].finished
+            let failed = state[0].failed
+            let status = { waiting: 0, active: 0, finished: success, failed: failed, id_process: data.id_process }
+            console.log(status);
+            await time(2000)
+            await processStateManager.updateState(status)
+            if (seeds.length == 0) {
+              processManager.processing({ action: 'kill', isp: seedsRunning[0].isp, id_process: data.id_process })
+            } else {
+              processManager.processing({ action: 'kill', isp: seeds[0].isp, id_process: data.id_process })
+            }
+            ip_process.shift()
           }
         }
-        ip_process.shift()
+        sendToAll(clients, 'location reload')
+        process.exit(0)
       }
-      sendToAll(clients, 'location reload')
-      process.exit(0)
     }
   })
 })
