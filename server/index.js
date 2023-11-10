8f5c741c37ec2408625d098718c757dceb845283
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-10 17:57:12 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -435,8 +435,8 @@ wss.on('connection', (wss, req) => {
     } else if (request == 'restart') {
       let ip_process = await processManager.getAllProcessByState({ status: "RUNNING" })
       if (ip_process.length == 0) {
-        var date = new Date();
-        console.log(date.split('T'));
+        var date = new Date().toString();
+        console.log(date);
         await time(5000)
         sendToAll(clients, 'location reload')
         process.exit(0)
