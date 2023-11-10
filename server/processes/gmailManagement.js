0944ccb2c87972098e428068eb8adee4d1bc7545
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-10 12:00:07 +0000

    adding functions

--- server/processes/gmailManagement.js
+++ server/processes/gmailManagement.js
@@ -108,7 +108,6 @@ const verify = async (data) => {
         return feedback
     }
     await navigationPromise
-    console.log('browser in ' + page.url());
     await time(2000)
     await page.click('#yDmH0d > c-wiz > div > div.eKnrVb > div > div.j663ec > div > form > span > section:nth-child(2) > div > div > section > div > div > div > ul > li:nth-child(3)')
     await time(2000)
@@ -135,8 +134,8 @@ const verify = async (data) => {
         await resultsManager.saveFeedback({ feedback: feedback, id_seeds: data.id_seeds, id_process: data.id_process })
         return feedback
     }
-    console.log('browser in ' + page.url());
     await page.goto('https://mail.google.com/mail/u/0/#inbox')
+    console.log(page.url());
     await navigationPromise
     console.log('verified email : ' + data.gmail);
     await page.screenshot({
@@ -148,84 +147,6 @@ const verify = async (data) => {
     await resultsManager.saveFeedback({ feedback: feedback, id_seeds: data.id_seeds, id_process: data.id_process })
     return feedback
 }
-
-// const verify = async (data) => {
-//     console.log(`opening seed : ${data.gmail}, At ${new Date().toLocaleString()}`);
-//     console.log(` `);
-
-//     let feedback = ''
-//     const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--single-process', '--no-zygote', '--disable-setuid-sandbox'] })
-//     const browserPID = browser.process().pid
-//     const page = await browser.newPage()
-//     pidProcess.push({ id_process: data.id_process, pid: browserPID })
-//     await page.setViewport({ width: 1280, height: 720 });
-//     const navigationPromise = page.waitForNavigation()
-//     await page.goto('https://gmail.com/')
-//     await navigationPromise
-//     await page.waitForSelector('input[type="email"]')
-//     await page.click('input[type="email"]')
-//     await navigationPromise
-//     await page.type('input[type="email"]', data.gmail, { delay: 100 })
-//     await page.waitForSelector('#identifierNext')
-//     await page.click('#identifierNext')
-//     await navigationPromise
-//     await time(10000)
-//     if (await page.$('[aria-invalid="true"]') != null || await page.$('#next > div > div > a') != null) {
-//         // await page.close()
-//         // await browser.close()
-//         console.log(`invalid email : ${data.gmail}`);
-//     }
-//     await navigationPromise
-//     await time(3000);
-//     try {
-//         await page.waitForSelector('input[type="password"]', { timeout: 500 })
-//     } catch (error) {
-//         if (error) {
-//             // await page.close()
-//             // await browser.close()
-//             console.log(`invalid email : ${data.gmail}`);
-//         }
-//     }
-//     await page.type('input[type="password"]', data.password, { delay: 200 })
-//     await time(1000)
-//     await Promise.all([
-//         page.$eval(`#passwordNext`, element =>
-//             element.click()
-//         ),
-//         await page.waitForNavigation(),
-//     ]);
-//     await time(1000)
-//     if (await page.$('[aria-invalid="true"]') != null) {
-//         console.log('invalid pass');
-//         // await page.close()
-//         // await browser.close()
-//     }
-//     await navigationPromise
-//     if (page.url() == 'https://mail.google.com/mail/u/0/#inbox') {
-//         console.log('verified email : ' + data.gmail);
-//         // await page.close()
-//         // await browser.close()
-//         return
-//     }
-//     await navigationPromise
-//     await page.click('#yDmH0d > c-wiz > div > div.eKnrVb > div > div.j663ec > div > form > span > section:nth-child(2) > div > div > section > div > div > div > ul > li:nth-child(3)')
-//     await time(2000)
-//     await page.type('#knowledge-preregistered-email-response', data.vrf, { delay: 100 })
-//     await page.waitForSelector('#view_container > div > div > div.pwWryf.bxPAYd > div > div.zQJV3 > div > div.qhFLie > div > div > button')
-//     await page.click('#view_container > div > div > div.pwWryf.bxPAYd > div > div.zQJV3 > div > div.qhFLie > div > div > button')
-//     await navigationPromise
-//     await time(2000)
-//     if (await page.$('[aria-invalid="true"]') != null) {
-//         console.log('invalid verification');
-//         // await page.close()
-//         // await browser.close()
-//         return
-//     }
-//     await page.goto('https://mail.google.com/mail/u/0/#inbox')
-//     await navigationPromise
-//     // await page.close()
-//     // await browser.close()
-// }
 
 const kill = (id_process) => {
     pidProcess.forEach(Element => {
@@ -244,7 +165,7 @@ const kill = (id_process) => {
     })
 }
 
-module.exports = {
+module.exports =
     verify,
     kill
-}
+
