162da368066d3bbb58c58533d38365250c5a4eb8
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-10 12:27:41 +0000

    adding functions

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -208,11 +208,11 @@ $(document).on('click', '.status', event => {
                     } else if (row.rstatus === null && row.pstatus === 'STOPPED') {
                         status = `<span class="text-danger">${row.pstatus}</span>`
                     } else if (row.rstatus == 'finished') {
-                        return `<div class="f-action card-body p-0 text-center">
+                        return `<div class="card-body p-0 text-center f-action">
                                 ${row.rstatus}
                             </div>`
                     } else if (row.rstatus == 'failed') {
-                        return `<div class="fr-action card-body p-0 text-center">
+                        return `<div class="card-body p-0 text-center fr-action">
                             ${row.rstatus}
                         </div>`
                     } else {
