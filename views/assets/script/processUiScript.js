d1e4beb925bc4193355dd29731e944f97b6e9f56
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-13 11:25:22 +0000

    adding functions

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -483,7 +483,25 @@ function timer() {
 }
 
 $(document).on('click', '.why', () => {
-    console.log('test');
+    let modal = `<div class="modal fade why-info" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+    $('footer').after(modal)
+    $('.why-info').modal('show')
 })
 
 $(document).on('click', '#restart_s', () => {
