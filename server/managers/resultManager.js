25bd7363b1a39da090d56524b1a9ce5bdf2f00af
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-14 11:00:09 +0000

    adding functions

--- server/managers/resultManager.js
+++ server/managers/resultManager.js
@@ -34,7 +34,7 @@ const endNow = async (data) => {
 }
 
 const saveFeedback = async (data) => {
-    let sql = `UPDATE results SET feedback+=($1) WHERE id_seeds=($2) AND id_process=($3)`
+    let sql = `UPDATE results SET feedback=($1) WHERE id_seeds=($2) AND id_process=($3)`
     let values = [data.feedback, data.id_seeds, data.id_process]
     const client = await pool.connect()
     client.query(sql, values, (err) => {
