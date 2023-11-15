d836ccf1563561f77a8f41f6efd26902cd31466f
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-15 11:29:43 +0000

    adding functions

--- server/processes/gmailManagement.js
+++ server/processes/gmailManagement.js
@@ -347,10 +347,6 @@ const markAsSpam = async (data) => {
     feedback += `, ${data.gmail.split('@')[0]}-@-login-${data.id_process}.png`
     await resultsManager.saveFeedback({ feedback: feedback, id_seeds: data.id_seeds, id_process: data.id_process })
     try {
-        // details += `Entre unread inbox : ${countInbox}`
-        // console.log(countInbox);
-        // await resultsManager.saveDetails({ details: details, id_seeds: data.id_seeds, id_process: data.id_process })
-
         const countEnter = await page.evaluate(() => {
             let html = []
             let el = document.querySelectorAll('.bsU')
@@ -363,24 +359,16 @@ const markAsSpam = async (data) => {
         console.log(countEnter);
         console.log(countEnter.length);
         console.log(countEnter[0].element);
-        if (countEnter[0].element != "Inbox" || countEnter[0].element != "Boîte de réception" || countEnter[0].element != "البريد الوارد") {
+        if (countEnter[0].element != "Inbox" && countEnter[0].element != "Boîte de réception" && countEnter[0].element != "البريد الوارد") {
             details += `Entre unread inbox : 0`
             console.log(details);
+            await resultsManager.saveDetails({ details: details, id_seeds: data.id_seeds, id_process: data.id_process })
+        } else {
+            details += `Entre unread inbox : ${countEnter[0].count}`
             await resultsManager.saveDetails({ details: details, id_seeds: data.id_seeds, id_process: data.id_process })
         }
     } catch (error) {
         console.log(error.message);
-        // console.log(error.message == 'Error: failed to find element matching selector ".bsU"');
-        // if (error.message == 'Error: failed to find element matching selector ".bsU"') {
-        //     details += `Entre unread inbox : 0`
-        //     console.log(details);
-        //     await resultsManager.saveDetails({ details: details, id_seeds: data.id_seeds, id_process: data.id_process })
-        // } else {
-        //     console.log(error);
-        // }
-        // details += `Entre unread inbox : 0`
-        // console.log(details);
-        // await resultsManager.saveDetails({ details: details, id_seeds: data.id_seeds, id_process: data.id_process })
     }
     await time(10000)
     await page.evaluate(() => {
