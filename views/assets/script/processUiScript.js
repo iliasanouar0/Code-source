f5fa0ace82597f8fb3fad7c60a6333288c2567b4
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-20 10:41:19 +0000

    adding functions

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -481,7 +481,6 @@ $(document).on('click', '.details', event => {
         let card = ""
         feedBack.forEach(element => {
             variables = element.split('-')
-            console.log(variables[2]);
             if (variables[2] == 'AUTO_LOGIN') {
                 card += `<div class="col">
                 <div class="card">
@@ -540,7 +539,6 @@ $(document).on('click', '#Refresh', event => {
         let card = ""
         feedBack.forEach(element => {
             variables = element.split('-')
-            console.log(variables[2]);
             if (variables[2] == 'AUTO_LOGIN') {
                 card += `<div class="col">
                 <div class="card">
