34659e44db4ba165c1515e548980b57e3777458d
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-23 16:15:37 +0000

    adding functions

--- views/assets/script/render_admin.js
+++ views/assets/script/render_admin.js
@@ -253,7 +253,7 @@ const getData = $("#example1").DataTable({
         // return `<div class="row">${html}</div>`
         return `<div class="card m-0 data-bs-toggle="tooltip" data-bs-title="${row.action}"">
           <div class="card-body p-0 text-center text-dark">
-          ${row.action.substring(0, 10)}
+          ${row.action.substring(0, 10)}...
           </div>
          </div>`
       }
