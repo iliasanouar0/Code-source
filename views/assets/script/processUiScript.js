562cc8883281eb216398921a6c4ee94de3bcc22e
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-24 12:40:17 +0000

    adding functions

--- views/assets/script/processUiScript.js
+++ views/assets/script/processUiScript.js
@@ -233,14 +233,18 @@ $(document).on('click', '.resume', event => {
     }
 })
 
-function msToMnSc(ms) {
-    var minutes = Math.floor(ms / 60000);
-    var seconds = ((ms % 60000) / 1000).toFixed(0);
-    let hours = 0
-    return (
-      minutes == 60 ? hours + 1 + ":" + seconds == 60 ? (minutes + 1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds : seconds == 60 ? (minutes + 1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds
-    );
-  }
+function msToMnSc(duration) {
+    var milliseconds = Math.floor((duration % 1000) / 100),
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
 
 $(document).on('click', '.status', event => {
     $('body').tooltip('dispose');
--- views/assets/script/render_admin.js
+++ views/assets/script/render_admin.js
@@ -130,13 +130,27 @@ fetch(adminNavbarUrl)
     navbarContainer.innerHTML = html;
   });
 
-function msToMnSc(ms) {
-  var minutes = Math.floor(ms / 60000);
-  var seconds = ((ms % 60000) / 1000).toFixed(0);
-  let hours = 0
-  return (
-    minutes == 60 ? hours + 1 + ":" + seconds == 60 ? (minutes + 1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds : seconds == 60 ? (minutes + 1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds
-  );
+// function msToMnSc(ms) {
+//   var minutes = Math.floor(ms / 60000);
+//   var seconds = ((ms % 60000) / 1000).toFixed(0);
+//   let hours = Math.floor(minutes / 60);
+//   return (
+//     hours + ":" + seconds == 60 ? (minutes + 1) + ":00" :
+//       minutes + ":" + (seconds < 10 ? "0" : "") + seconds
+//   );
+// }
+
+function msToMnSc(duration) {
+  var milliseconds = Math.floor((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
 }
 
 const getData = $("#example1").DataTable({
