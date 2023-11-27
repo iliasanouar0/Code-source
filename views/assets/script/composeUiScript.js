fa5117e0c74ad0709ff8b7032a1b11aaab7d672e
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-27 17:00:10 +0000

    adding functions

--- views/assets/script/composeUiScript.js
+++ views/assets/script/composeUiScript.js
@@ -235,14 +235,14 @@ $(document).on('click', '.resume', event => {
 
 function msToMnSc(duration) {
     var milliseconds = Math.floor((duration % 1000) / 100),
-      seconds = Math.floor((duration / 1000) % 60),
-      minutes = Math.floor((duration / (1000 * 60)) % 60),
-      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-  
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
-  
+
     return hours + ":" + minutes + ":" + seconds;
 }
 
@@ -868,5 +868,8 @@ $('#btn-check-compose').change(event => {
     if (status) {
         $('.btn-check').prop("checked", false);
         $(event.target).prop("checked", true);
+        $('.send_message').removeClass('d-none');
+    } else {
+        $('.send_message').addClass('d-none');
     }
 })
