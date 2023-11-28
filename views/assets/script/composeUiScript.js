9b5bb8de3a2a020811efac1d6c9b1d61856657f0
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-28 15:08:26 +0000

    adding functions

--- views/assets/script/composeUiScript.js
+++ views/assets/script/composeUiScript.js
@@ -96,24 +96,6 @@ $(document).on('click', '.upload', event => {
 })
 
 const addCompose = data => {
-
-    // var myHeaders = new Headers();
-    // myHeaders.append("Content-Type", "application/json");
-
-    // var raw = JSON.stringify(data);
-
-    // var requestOptions = {
-    //     method: 'POST',
-    //     headers: myHeaders,
-    //     body: raw,
-    //     redirect: 'follow'
-    // };
-
-    // fetch(`http://${ip}:3000/compose/`, requestOptions)
-    //     .then(response => response.text())
-    //     .then(result => console.log(result))
-    //     .catch(error => console.log('error', error));
-
     var settings = {
         "url": "http://project1.gm2reporting.com:3000/compose/",
         "method": "POST",
@@ -121,16 +103,7 @@ const addCompose = data => {
         "headers": {
             "Content-Type": "application/json"
         },
-        "data": JSON.stringify({
-            "name": "test",
-            "action": "checkProxy",
-            "status": "idel",
-            "id_user": "20",
-            "id_list": "50",
-            "offer": "none",
-            "data": "none",
-            "count": "none"
-        }),
+        "data": JSON.stringify(data),
     };
 
     $.ajax(settings).done(function (response) {
@@ -147,30 +120,6 @@ const addCompose = data => {
             getDataCompose.ajax.reload(null, false)
         })
     });
-    // fetch("", {
-    //     method: 'POST',
-    //     body: `${JSON.stringify(data)}`,
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'Access-Control-Allow-Origin': '*',
-    //         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
-    //         'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS'
-    //     }
-    // }).then(response => {
-    //     return response.text()
-    // }).then(data => {
-    //     Swal.fire({
-    //         title: data,
-    //         timer: 1500,
-    //         showConfirmButton: false,
-    //         icon: 'success'
-    //     }).then(() => {
-    //         $('.add_compose input:text').val('')
-    //         $('.add_compose input:checkbox').prop("checked", false)
-    //         $(".add_compose").modal("hide");
-    //         getDataCompose.ajax.reload(null, false)
-    //     })
-    // })
 }
 
 $('#p_offers_add').change(event => {
@@ -214,7 +163,7 @@ $(document).on('click', '#c_add', () => {
                 "id_list": `${composingList}`,
                 "offer": `none`,
                 "data": `none`,
-                "count": `none`,
+                "count": 0,
             };
             console.log(dataComposing);
 
@@ -229,7 +178,7 @@ $(document).on('click', '#c_add', () => {
                 "id_list": `${composingList}`,
                 "offer": `none`,
                 "data": `none`,
-                "count": `none`,
+                "count": 0,
             };
             console.log(dataComposing);
 
