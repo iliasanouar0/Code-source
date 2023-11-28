18d2d27bdf58a6212cb3c098634b21cbfe3e9b5c
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-28 13:19:31 +0000

    adding functions

--- views/assets/script/composeUiScript.js
+++ views/assets/script/composeUiScript.js
@@ -101,7 +101,7 @@ const addCompose = data => {
         body: `${JSON.stringify(data)}`,
         headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
+            // 'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
             'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS'
         }
