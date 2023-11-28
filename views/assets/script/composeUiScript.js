3acc9d9f139a1720ee9186deaeb72fda66c765c6
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-28 12:49:57 +0000

    adding functions

--- views/assets/script/composeUiScript.js
+++ views/assets/script/composeUiScript.js
@@ -198,7 +198,7 @@ $(document).on('click', '#c_add', () => {
                 var settings = {
                     "url": `http://${ip}:3000/compose/offers?offer=${offerAdd}`,
                     "method": "POST",
-                    "data": `${data}`,
+                    "data": JSON.parse({ data: data }),
                     "timeout": 0,
                 };
                 console.log(data);
