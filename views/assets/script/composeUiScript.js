0744800f52a8fde921028c9be5d7f671464e6449
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-28 13:23:32 +0000

    adding functions

--- views/assets/script/composeUiScript.js
+++ views/assets/script/composeUiScript.js
@@ -99,6 +99,12 @@ const addCompose = data => {
     fetch(`http://${ip}:3000/compose/`, {
         method: 'POST',
         body: `${JSON.stringify(data)}`,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
+            'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS'
+        }
     }).then(response => {
         return response.text()
     }).then(data => {
