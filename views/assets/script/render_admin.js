8f19f3c1e52d156961b18dcfac2060e4700ab659
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-28 15:15:31 +0000

    adding functions

--- views/assets/script/render_admin.js
+++ views/assets/script/render_admin.js
@@ -448,7 +448,7 @@ const getDataCompose = $("#composeData").DataTable({
       data: null,
       render: function (data, type, row) {
         if (row.status == 'idel' || row.status == 'STOPPED') {
-          return `${row.date_add} <span class="text-danger">(Create at)</span>`
+          return `${row.add_date} <span class="text-danger">(Create at)</span>`
         }
         let start_in = new Date(row.start_in)
         let start = `${start_in.toLocaleString()}`
