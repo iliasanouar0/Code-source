5cfdfbb5be1fde51282adab67b209397e5767966
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-29 17:35:30 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -759,12 +759,11 @@ wsc.on('connection', (wss, req) => {
               }
               if (toProcess.length < active && count < length && state != "STOPPED") {
                 seeds = await composeManager.getAllProcessSeedsByState({ id_process: data.id_process, status: "waiting" })
-                let index = randomRange(0,100)
-                toProcess.push(seeds[index])
+                toProcess.push(seeds[seeds.length - i])
                 await time(3000)
-                await resultManager.startNow({ id_seeds: seeds[index].id_seeds, id_process: data.id_process })
+                await resultManager.startNow({ id_seeds: seeds[seeds.length - i].id_seeds, id_process: data.id_process })
                 await time(3000)
-                await resultManager.updateState([{ id_seeds: seeds[index].id_seeds, id_process: data.id_process }], "running")
+                await resultManager.updateState([{ id_seeds: seeds[seeds.length - i].id_seeds, id_process: data.id_process }], "running")
                 await time(3000)
                 count++
                 let w = waiting - count + 3
