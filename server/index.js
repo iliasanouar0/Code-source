cd2bbb410f38a359ca27d0774b3a64dad75e5b79
iliasanouar0 <iliasanouar0@gmail.com>
2023-11-30 09:13:02 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -827,8 +827,8 @@ wsc.on('connection', (wss, req) => {
       }
 
       async function repeat(array, number, start) {
-        console.log(start);
         process(array[start], start)
+        console.log(number > start);
         if (number > start) await repeat(array, number, start + 1);
       }
 
