31151fe163c3520f6dd9daa4acb6ff7700c06b9e
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-01 15:01:33 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -895,7 +895,7 @@ wsc.on('connection', (wss, req) => {
           for (let i = 0; i < array[start].length; i++) {
             await resultManager.startNow({ id_seeds: array[start][i].id_seeds, id_process: data.id_process })
             await resultManager.updateState([{ id_seeds: array[start][i].id_seeds, id_process: data.id_process }], "running")
-            process([array[start][i]], start, { onlyStarted: false })
+            process([array[start][i]], i, { onlyStarted: false })
           }
         } else {
           await time(3000)
