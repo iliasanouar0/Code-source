335067063553b1b7297a32b7ebedf75e194a863e
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-05 15:59:00 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -693,6 +693,7 @@ wsc.on('connection', (wss, req) => {
         actions = [actions]
       } else {
         actions = actions.split(',')
+        console.log(actions);
         let length = actions.length
         for (let i = 0; i < length; i++) {
           console.log(actions[length - (i + 1)].indexOf('subject') != -1);
