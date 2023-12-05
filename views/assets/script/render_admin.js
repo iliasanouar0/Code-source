f12baae6d6ea206aecec66c5037d6cd99c0efd2c
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-05 09:05:29 +0000

    adding functions

--- views/assets/script/render_admin.js
+++ views/assets/script/render_admin.js
@@ -1098,7 +1098,7 @@ if (path.includes("/admin/users/")) {
         offersAdd.appendChild(option);
         data.forEach((elm) => {
           let option = document.createElement("option");
-          option.append('<button>test</button>')
+          option.innerHTML('<button>test</button>')
           // option.innerHTML = `${elm['file']}`
           option.setAttribute("value", elm['file']);
           offersAdd.appendChild(option);
