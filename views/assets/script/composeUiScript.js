0fbe0f9541237ca8e534d177a77edebfefb5fc1e
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-06 16:11:02 +0000

    adding functions

--- views/assets/script/composeUiScript.js
+++ views/assets/script/composeUiScript.js
@@ -70,6 +70,7 @@ $(document).on('click', '.edit', event => {
         let list = $('#p_list_add').children()
         for (let i = 0; i < list.length; i++) {
             if ($(list[i]).val() == json[0].id_list) {
+                console.log($(list[i]).val());
                 $(list[i]).attr('selected', true)
                 break
             }
