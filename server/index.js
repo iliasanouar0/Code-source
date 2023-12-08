f767a02166cf499a1c1dcc4994228fe57d269147
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-08 16:19:05 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -1401,6 +1401,7 @@ wsc.on('connection', (wss, req) => {
       await time(5000)
       let check = { startingIndexed: toProcess.length == 3 ? false : true }
       await repeat(toProcess, bccToProcess, toProcess.length, 0, check.startingIndexed, actions[0])
+      await time(5000)
       let status = { waiting: waiting, active: active, finished: 0, failed: 0, id_process: data.id_process }
       processStateManager.addState(status)
 
