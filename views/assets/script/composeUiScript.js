f5c1d8fec1db549729509a6ba40dcb8275283365
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-08 12:05:01 +0000

    adding functions

--- views/assets/script/composeUiScript.js
+++ views/assets/script/composeUiScript.js
@@ -484,23 +484,21 @@ $(document).on('click', '#c_add', () => {
                 }
             } else {
                 let time = new Date().toTimeString().split(' ')[0]
-                console.log(time);
                 offerAdd = `${subject.substring(0, 3)}${user['id_user']}offer${time}.html`
-                console.log(offerAdd);
-                // fetch(`http://${ip}:3000/compose/offers?offer=${offerAdd}`, {
-                //     method: 'POST',
-                //     body: `${JSON.stringify({ data: body })}`,
-                //     headers: {
-                //         'Content-Type': 'application/json',
-                //         'Access-Control-Allow-Origin': '*',
-                //         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
-                //         'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS'
-                //     }
-                // }).then(r => {
-                //     return r.text()
-                // }).then(d => {
-                //     console.log(d);
-                // })
+                fetch(`http://${ip}:3000/compose/offers?offer=${offerAdd}`, {
+                    method: 'POST',
+                    body: `${JSON.stringify({ data: body })}`,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
+                        'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS'
+                    }
+                }).then(r => {
+                    return r.text()
+                }).then(d => {
+                    console.log(d);
+                })
             }
             action += `,subject:${subject},to:${to}`
             if (limit != '' && limit != '0') {
@@ -521,8 +519,7 @@ $(document).on('click', '#c_add', () => {
                 "data": `${data}`,
                 "count": `${count}`,
             };
-            console.log(dataComposing);
-            // addCompose(dataComposing)
+            addCompose(dataComposing)
             break;
         default:
             break;
