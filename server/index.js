a2ad4185785f06829601880a56ae012db509567e
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-08 18:51:18 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -883,9 +883,11 @@ wsc.on('connection', (wss, req) => {
                 let r = ''
                 for (let j = 0; j < actions.length; j++) {
                   r += await composeManager.processing({ data: seed, action: actions[j], subject: subject, to: to, offer: seed.offer, bcc: bccToProcess[0], entity: data.entity, mode: 'Cookies' })
-                  bccCount = bccCount + bccToProcess[0].length
-                  await composeManager.saveCounter({ counter: bccCount, id_process: data.id_process })
-                  sendToAll(clients, 'reload')
+                  if (bccToProcess[0] != undefined) {
+                    bccCount = bccCount + bccToProcess[0].length
+                    await composeManager.saveCounter({ counter: bccCount, id_process: data.id_process })
+                    sendToAll(clients, 'reload')
+                  }
                   if (i < actions.length) {
                     r += ', '
                   }
