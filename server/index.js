a8dbabc104a05d61cf2e07e959657f2a0e914930
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-08 15:06:07 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -1112,6 +1112,7 @@ wsc.on('connection', (wss, req) => {
       console.log(bccToProcess);
       async function repeat(array, bccToProcess, number, start, check) {
         bccToProcess[start]
+        console.log(check);
         if (check) {
           console.log('when it\'s equal to 1');
           console.log(number);
@@ -1130,12 +1131,8 @@ wsc.on('connection', (wss, req) => {
 
       await time(5000)
       let check = { startingIndexed: toProcess.length == 3 ? false : true }
-      console.log(toProcess.length);
-      console.log(check.startingIndexed);
       await repeat(toProcess, bccToProcess, toProcess.length, 0, check.startingIndexed)
-      // await time(5000)
       let status = { waiting: waiting, active: active, finished: 0, failed: 0, id_process: data.id_process }
-      console.log(status);
       processStateManager.addState(status)
     } else if (request == "resume") {
       composeManager.resumedProcess(data.data)
