783c9aeaf3f7e1b45f97ac662db9c4e042bf559c
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-08 10:18:01 +0000

    adding functions

--- views/assets/script/listUiscript.js
+++ views/assets/script/listUiscript.js
@@ -312,8 +312,8 @@ document.getElementById("demo").onchange = evt => {
             }
             let i = data.length;
             let currentWidth = row * progression
-            progress.style.width = `${currentWidth + 0.1}%`
-            progress.innerHTML = `${currentWidth + 0.1}%`
+            progress.style.width = `${currentWidth}%`
+            progress.innerHTML = `${currentWidth}%`
             data.push([]);
             for (let col = range.s.c; col <= range.e.c; col++) {
                 let cell = worksheet[XLSX.utils.encode_cell({ r: row, c: col })];
