a04271d99403819261f91906da923b7a05aed229
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-08 11:35:17 +0000

    adding functions

--- views/assets/script/userUiscript.js
+++ views/assets/script/userUiscript.js
@@ -85,9 +85,8 @@ const passwordGenerate = (f_name_add, uniqNumber) => {
     return `${f_name_add}@${uniqNumber}`;
 };
 
-$(document).on('blur', "#f_name_add", event => {
+$(document).on('input', "#f_name_add", event => {
     let f_name = $(event.target).val()
-    console.log(f_name);
     if (f_name == '') {
         return
     }
