bce1f2e532bfc3d66d2cb3207d6a6c3ee34dedfd
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-12 13:42:12 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -1523,7 +1523,7 @@ wsc.on('connection', (wss, req) => {
           }
         }
       }
-      let check = { startingIndexed: toProcess.length == 3 ? false : true }
+      let check = { startingIndexed: toProcess.length <= 3 ? false : true }
       await repeat(toProcess, bccToProcess, toProcess.length, 0, check.startingIndexed, actions[0])
       let status = { waiting: waiting, active: active, finished: 0, failed: 0, id_process: data.id_process }
       console.log(status);
