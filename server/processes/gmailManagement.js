0009a13f6a41964e9b3d94e09896ccd313049c6b
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-12 15:12:24 +0000

    adding functions

--- server/processes/gmailManagement.js
+++ server/processes/gmailManagement.js
@@ -168,7 +168,7 @@ const verify = async (data, entity, mode) => {
         pidProcess.push({ id_process: data.id_process, pid: browserPID })
         await page.setViewport({ width: 1280, height: 720 });
         await page.setDefaultNavigationTimeout(60000)
-        const navigationPromise = page.waitForNavigation({ timeout: 3000 })
+        const navigationPromise = page.waitForNavigation({ timeout: 30000 })
 
         await page.goto('https://gmail.com/')
 
