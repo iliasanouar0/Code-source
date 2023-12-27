0402412f1410bfea83c3c0d768bb666f83fd1bfe
iliasanouar0 <iliasanouar0@gmail.com>
2023-12-27 09:35:01 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -800,7 +800,7 @@ wsc.on('connection', (wss, req) => {
 
         counter = parseInt(await composeManager.getCounter(data.id_process))
         console.log('counter : ' + counter);
-        if (counter != 0 && counter != null && counter != NaN) {
+        if (counter != 0) {
           arrayBcc.slice(0, counter)
         } else {
           counter = 0
