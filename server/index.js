cf5adaf5945ef943aeb4949908ec2fedae44f408
iliasanouar0 <iliasanouar0@gmail.com>
2024-01-02 10:43:31 +0000

    adding functions

--- server/index.js
+++ server/index.js
@@ -1646,7 +1646,7 @@ wsc.on('connection', (wss, req) => {
               }
 
               console.log("seedsRunning.length : " + seedsRunning.length);
-              if (Origins.length == 0 && seeds.length === 0 && seedsRunning.length === 0) {
+              if (bccResult.length == 0 && seeds.length === 0 && seedsRunning.length === 0) {
                 let status = { waiting: 0, active: 0, finished: success, failed: failed, id_process: data.id_process };
                 await processStateManager.updateState(status);
                 composeManager.finishedProcess({ id_process: data.id_process, status: `FINISHED` });
