66057f4a10d1bfba3321cc718e261bdccff0411a
iliasanouar0 <iliasanouar0@gmail.com>
2024-01-04 12:28:27 +0000

    adding functions

--- views/assets/script/cloudProjectUiScript.js
+++ views/assets/script/cloudProjectUiScript.js
@@ -1,5 +1,15 @@
 
 
 $(document).on('click', '#add_Project', e => {
+    let settings = {
+        url: `http://${ip}:3000/cloud/account/project`,
+        method: "GET",
+        timeout: 0,
+        data: data,
+    }
+    $.ajax(settings).done(function (responseText) {
+        console.log(responseText);
+    })
+    // cloudaccount
     $('.add_Project').modal('show')
 })
